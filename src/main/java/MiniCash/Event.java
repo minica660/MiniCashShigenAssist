4f364d2cded72dd4421d9c6e8143d3eb32b4225c
MiniCash <tminecraft2143@gmail.com>
2026-04-22 21:18:41 +0900

    Update Event.java

--- src/main/java/MiniCash/Event.java
+++ src/main/java/MiniCash/Event.java
@@ -72,8 +72,7 @@ public class Event implements Listener {
                 config.set("elytra",false);
                 config.save(playerFile);
 
-                player.sendMessage("[§aMiniCashShigenAssist§r] §6データファイルを作成しました");
-                plugin.getLogger().info(player.getName() + " のデータファイルを作成しました！");
+a                plugin.getLogger().info(player.getName() + " のデータファイルを作成しました！");
 
             } catch (IOException e) {
                 player.sendMessage(e.getMessage());
